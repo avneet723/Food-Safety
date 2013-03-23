2b1e0aa0ff260b812ddca0d1048c1bc9e9efa5cc
Evan Tatarka <evan@tatarka.me>
2013-03-23 16:36:57 -0400

    changed handwashing station images

--- src/HandwashingScreen.js
+++ src/HandwashingScreen.js
@@ -6,18 +6,32 @@ import animate;
 import ui.View;
 import ui.ImageView;
 import ui.TextView;
+import ui.widget.ButtonView;
  
-exports = Class(ui.ImageView, function (supr) {
+exports = Class(ui.View, function (supr) {
   var self = this;
 
   this.init = function (app) {
     self.app = app;
+    supr(this, 'init');
+  };
 
-    opts = {
-      image: "resources/images/HandwashingScreen.png"
-    };
+  this.buildView = function() {
+    new ui.ImageView({
+      superview: this,
+      width: 800,
+      height: 600,
+      image: "resources/images/handwashing.png"
+    });
 
-    supr(this, 'init', [opts]);
+    var waterStream = new ui.ImageView({
+      superview: this,
+      x: 347,
+      y: 249,
+      width: 16,
+      height: 115,
+      image: "resources/images/water-stream.png"
+    });
   };
 
   this.onInputSelect = function() {
